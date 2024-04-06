ec2247a5016c8b9da93a74b77e389b095303d698
Kriti Sodhi <kritisodhi@Kritis-Air.phub.net.cable.rogers.com>
2024-04-06 11:48:17 -0400

    test deploy

--- front-end/src/pages/referencesPage.jsx
+++ front-end/src/pages/referencesPage.jsx
@@ -1,31 +1,32 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
+import jobPostingData from "../data/jobPostingtext";
 
 function ReferencesPage() {
-  const [jobPostingData, setJobPostingData] = useState([]);
-  const [screeningData, setScreeningData] = useState([]);
-  const [interviewData, setInterviewData] = useState([]);
-  const [postSelectionData, setPostSelectionData] = useState([]);
+  // const [jobPostingData, setJobPostingData] = useState([]);
+  // const [screeningData, setScreeningData] = useState([]);
+  // const [interviewData, setInterviewData] = useState([]);
+  // const [postSelectionData, setPostSelectionData] = useState([]);
 
 
-  useEffect(() => {
-    Promise.all([
-      fetch("http://localhost:3000/api/references/jobposting"),
-      fetch("http://localhost:3000/api/references/screening"),
-      fetch("http://localhost:3000/api/references/interview"),
-      fetch("http://localhost:3000/api/references/postselection")
-    ])
-      .then((responses) => Promise.all(responses.map((res) => res.json())))
-      .then(([jobPostingData, screeningData, interviewData, postSelectionData ]) => {
-        setJobPostingData(jobPostingData);
-        setScreeningData(screeningData);
-        setInterviewData(interviewData);
-        setPostSelectionData(postSelectionData);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, 
-  []);
+  // useEffect(() => {
+  //   Promise.all([
+  //     fetch("http://localhost:3000/api/references/jobposting"),
+  //     fetch("http://localhost:3000/api/references/screening"),
+  //     fetch("http://localhost:3000/api/references/interview"),
+  //     fetch("http://localhost:3000/api/references/postselection")
+  //   ])
+  //     .then((responses) => Promise.all(responses.map((res) => res.json())))
+  //     .then(([jobPostingData, screeningData, interviewData, postSelectionData ]) => {
+  //       setJobPostingData(jobPostingData);
+  //       setScreeningData(screeningData);
+  //       setInterviewData(interviewData);
+  //       setPostSelectionData(postSelectionData);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, 
+  // []);
 
   return (
     <div className="References Page bg-gray-900 min-h-screen text-neutral-100 text-left flex flex-col space-y-8">
@@ -38,29 +39,30 @@ function ReferencesPage() {
             ))}
           </div>
         </div>
+                    <img src="../../../assets/logo.png" className="mr-3 h-6 sm:h-9" alt="Logo" />
         <div>
         <h1 className='text-center font-extrabold text-pink-400'>Screening References</h1>
-          <div className='flex flex-col space-y-4'>
+          {/* <div className='flex flex-col space-y-4'>
             {screeningData.map((item) => (
               <p key={item.id}>{item.text}</p>
             ))}
-          </div>
+          </div> */}
         </div>
         <div>
         <h1 className='text-center font-extrabold text-pink-400'>Interviewing References</h1>
-          <div className='flex flex-col space-y-4'>
+          {/* <div className='flex flex-col space-y-4'>
             {interviewData.map((item) => (
               <p key={item.id}>{item.text}</p>
             ))}
-          </div>
+          </div> */}
         </div>
         <div>
         <h1 className='text-center font-extrabold text-pink-400'>Selection and Post-Selection References</h1>
-          <div className='flex flex-col space-y-4'>
+          {/* <div className='flex flex-col space-y-4'>
            {postSelectionData.map((item) => (
             <p key={item.id}>{item.text}</p>
           ))} 
-          </div>
+          </div> */}
           
         </div>
     </div>
